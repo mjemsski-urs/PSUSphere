66b9ceb5b2481021bfa68dbd08ba693cad92a21c
aeprnts <cessywp16@gmail.com>
2026-03-02 21:33:43 +0800

    Added Student, Program, and Collegs, fixed admin list_display, updated templates to show Student ID, Program, and College

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -1,11 +1,14 @@
+# studentorg/models.py
+
 from django.db import models
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
+
 
 class College(BaseModel):
     college_name = models.CharField(max_length=150)
@@ -15,10 +18,11 @@ class College(BaseModel):
 
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150)
-    college = models.ForeignKey(College, on_delete=models.CASCADE)
+    college = models.ForeignKey("College", on_delete=models.CASCADE)  # string reference works too
 
     def __str__(self):
         return self.prog_name
+
 
 class Organization(BaseModel):
     name = models.CharField(max_length=250)
@@ -29,19 +33,19 @@ class Organization(BaseModel):
         return self.name
 
 class Student(BaseModel):
-    student_id = models.CharField(max_length=15)
-    lastname = models.CharField(max_length=25)
-    firstname = models.CharField(max_length=25)
+    student_number = models.CharField(max_length=20, blank=True, null=True)
+    lastname = models.CharField(max_length=25, blank=True, null=True)
+    firstname = models.CharField(max_length=25, blank=True, null=True)
     middlename = models.CharField(max_length=25, blank=True, null=True)
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.lastname}, {self.firstname}"
+        return f"{self.student_number or 'NoNumber'} - {self.lastname}, {self.firstname}"
 
 class OrgMember(BaseModel):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    date_joined = models.DateField()
+    date_joined = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.student}"
+        return f"{self.student} in {self.organization}"
